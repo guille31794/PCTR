2e61796c6514e0efaaba13fb587933cea4642b50
Guille31794 <guille.g94@hotmail.es>
2018-09-30 11:43:50 +0200

    Realizado el ejercicio, pero arroja una excepción de la clase Scanner

--- Practicas/Practica 2/Ej 1/Biblioteca.java
+++ Practicas/Practica 2/Ej 1/Biblioteca.java
@@ -55,7 +55,7 @@ public class Biblioteca
     return new Paciente();
   }
 
-  public Paciente consultTele(int patron)
+  public Paciente consultTele(long patron)
   {
     if(!colection.isEmpty())
     {
@@ -69,15 +69,14 @@ public class Biblioteca
     return new Paciente();
   }
 
-  /** Menus to satisfy options */
   private void menuInsert()
   {
     Paciente p;
     String n, d, a, ic;
     int t;
-    Scanner sc = new Scanner(System.in);
     char c;
-
+    try(Scanner sc = new Scanner(System.in))
+    {
     System.out.println("Insert patient's name: ");
     n = sc.nextLine();
     System.out.println("Insert IDN: ");
@@ -99,48 +98,107 @@ public class Biblioteca
       p = new Paciente(n, d, a, t, ic);
       insert(p);
     }
-
+  }
   }
 
   private void menuErase()
   {
+    int option;
+    char c;
+    Paciente p = new Paciente();
+    System.out.println("Erase Menu, select option:");
+    System.out.println("1.- Erase by name");
+    System.out.println("2.- Erase by IDN");
+    System.out.println("3.- Erase by telephone number");
+    try(Scanner sc = new Scanner(System.in))
+    {
+      option = sc.nextInt();
+
+      switch(option)
+      {
+        case 1: System.out.println("Insert name: ");
+        p = consultName(sc.nextLine());
+        break;
+        case 2: System.out.println("Insert IDN: ");
+        p = consultDni(sc.nextLine());
+        break;
+        case 3: System.out.println("Insert telephone number: ");
+        p = consultTele(sc.nextLong());
+        break;
+        default: break;
+      }
+      System.out.println("Are you sure you want to delete? y/n");
+      System.out.println(p.getName() + " " + p.getDni() + " " + p.getTelephoneN());
+      System.out.println(p.getAdress() + " " + p.getInsuranceC());
+      c = sc.next().trim().charAt(0);
+      if(c == 'y' || c == 'Y')
+      {
+        delete(p);
+      }
+    }
 
   }
 
   private void menuQuery()
   {
-
-  }
-
-  public void menu()
-  {
-    Biblioteca query = new Biblioteca();
     int option;
-    Scanner sc = new Scanner(System.in);
-
-    do
+    Paciente p = new Paciente();
+    System.out.println("Query Menu, select option:");
+    System.out.println("1.- Consult by name");
+    System.out.println("2.- Consult by IDN");
+    System.out.println("3.- Consult by telephone number");
+    try(Scanner sc = new Scanner(System.in))
     {
-      System.out.println("Welcome to your query management");
-      System.out.println("Select the option to perform");
-      System.out.println("1.- Insert patient");
-      System.out.println("2.- Erase patient");
-      System.out.println("3.- Consult patient");
-      System.out.println("0.- Exit program");
       option = sc.nextInt();
 
       switch(option)
       {
-        case 1: menuInsert();
+        case 1: System.out.println("Insert name: ");
+        p = consultName(sc.nextLine());
         break;
-        case 2: menuErase();
+        case 2: System.out.println("Insert IDN: ");
+        p = consultDni(sc.nextLine());
         break;
-        case 3: menuQuery();
+        case 3: System.out.println("Insert telephone number: ");
+        p = consultTele(sc.nextLong());
         break;
-        case 0: System.out.println("Skipping...");
-        System.exit(-1);
+        default: break;
       }
+    }
+    System.out.println("Search results: ");
+    System.out.println(p.getName() + " " + p.getDni() + " " + p.getTelephoneN());
+    System.out.println(p.getAdress() + " " + p.getInsuranceC());
+  }
 
-    } while(option != 0);
+  public void menu()
+  {
+    int option;
+    try(Scanner sc = new Scanner(System.in);)
+    {
+      do
+      {
+        System.out.println("Welcome to your query management");
+        System.out.println("Select the option to perform");
+        System.out.println("1.- Insert patient");
+        System.out.println("2.- Erase patient");
+        System.out.println("3.- Consult patient");
+        System.out.println("0.- Exit program");
+        option = sc.nextInt();
+
+        switch(option)
+        {
+          case 1: menuInsert();
+          break;
+          case 2: menuErase();
+          break;
+          case 3: menuQuery();
+          break;
+          case 0: System.out.println("Skipping...");
+          System.exit(-1);
+        }
+
+      } while(option != 0);
+    }
   }
 
   public static void main(String[] args)
@@ -148,5 +206,4 @@ public class Biblioteca
     Biblioteca library = new Biblioteca();
     library.menu();
   }
-
 }
