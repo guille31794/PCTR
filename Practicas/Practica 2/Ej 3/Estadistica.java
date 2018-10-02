45617c547ed521da4f1fcd5eaad474408f13ad44
Guille31794 <guille.g94@hotmail.es>
2018-10-02 10:10:16 +0200

    Nuevo ejercicio que tira excepcion de la clase scanner

--- Practicas/Practica 2/Ej 3/Estadistica.java
+++ Practicas/Practica 2/Ej 3/Estadistica.java
@@ -6,7 +6,8 @@
 * también por el usuario
 */
 
-import java.util.Scanner;
+import java.util.*;
+import java.lang.Math;
 
 public class Estadistica
 {
@@ -20,49 +21,85 @@ public class Estadistica
 
   public void insertElem()
   {
-    Scanner sc = new Scanner(System.in);
-    for(int i = 0; i < size; ++i)
+    try(Scanner sc = new Scanner(System.in))
     {
-      System.out.println("Introduzca elemento " + (i+1));
-      vector[i] = sc.nextInt();
-      //sc.nextLine();
+      for(int i = 0; i < size; ++i)
+      {
+        System.out.println("Introduzca elemento " + (i+1));
+        vector[i] = sc.nextInt();
+        //sc.nextLine();
+      }
     }
   }
 
   public double media()
   {
+    int sum = 0;
+    for(int i = 0; i < size; ++i)
+      sum += vector[i];
 
+    return (sum/size);
   }
 
-  public double moda()
+  public int moda()
   {
+    int frecuency = 0, acumulatedFrecuency = 0, moda = 0;
 
+    for(int i = 0; i < size; ++i)
+    {
+      for(int j = 0; j < size; ++j)
+      {
+        if(vector[i] == vector[j])
+         ++frecuency;
+      }
+      if(frecuency > acumulatedFrecuency)
+        moda = vector[i];
+    }
+
+    return moda;
   }
 
   public double varianza()
   {
+    double media = this.media();
+    double varianza, sum = 0, aux = 0;
 
+    for(int i = 0; i < size; ++i)
+    {
+      aux = vector[i] - media;
+      aux = Math.pow(aux, 2);
+      sum += aux;
+    }
+
+    varianza = sum / size;
+
+    return varianza;
   }
 
   public double desviacion()
   {
-    
+    double desviacion = this.varianza();
+    desviacion = Math.sqrt(desviacion);
+
+    return desviacion;
   }
 
   public static void main(String[] args)
   {
     int option;
-    Scanner sc = new Scanner(System.in);
     Estadistica es = new Estadistica(Integer.parseInt(args[0]));
     es.insertElem();
 
-    System.out.println("Menu estadistico");
-    System.out.println("1.- Media");
-    System.out.println("2.- Moda");
-    System.out.println("3.- Varianza");
-    System.out.println("4.- Desviacion tipica");
-    System.out.println("Otra.- Salir");
-    option = sc.nextInt();
+    try(Scanner sc = new Scanner(System.in))
+    {
+      System.out.println("Menu estadistico");
+      System.out.println("1.- Media");
+      System.out.println("2.- Moda");
+      System.out.println("3.- Varianza");
+      System.out.println("4.- Desviacion tipica");
+      System.out.println("Otra.- Salir");
+      option = sc.nextInt();
+    }
 
     switch(option)
     {
