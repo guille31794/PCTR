0116dc2a908aac84d9c3e6abd9a3d4bce6033d26
guille31794 <guille.g94@hotmail.es>
2018-10-04 13:08:35 +0200

    Otro ejercicio que no puedo testar por la clase scanner

--- Practicas/Practica 2/Ej 4/Complejos.java
+++ Practicas/Practica 2/Ej 4/Complejos.java
@@ -1,7 +1,67 @@
+/**
+* @author Guillermo Girón Garcíab
+* @version 1.0
+* Clase que modela numeros Complejoss junto con sus operaciones mas habituales.
+* El grado de la parte imaginaria siempre será 1.
+*/
+
+import java.lang.Math;
 
 public class Complejos
 {
-  private int r_, i_;
+  private double r_, i_;
   public Complejos()  { r_ = 0; i_ = 0;}
-  public Complejos(int r, int i)  {r_ = r; i_ = i;}
+  public Complejos(double r, double i)  {if(r >= 0) r_ = r; else r_ = 0; if(i >= 0)
+     i_ = i; else i_ = 0;}
+  public double getReal()  { return r_;}
+  public double getImaginary() { return i_;}
+  public Complejos Sum(Complejos C)
+  {
+    Complejos returned = new Complejos();
+    returned.r_ += this.r_; returned.r_ += C.r_;
+    returned.i_ += this.i_; returned.i_ += C.i_;
+    return returned;
+  }
+  public Complejos Subs(Complejos C)
+  {
+    Complejos returned = new Complejos();
+    returned.r_ -= this.r_; returned.r_ -= C.r_;
+    returned.i_ -= this.i_; returned.i_ -= C.i_;
+    return returned;
+  }
+  public double Mod()
+  {
+    double z = 0;
+    z += (this.getReal() * this.getReal());
+    z += (this.getImaginary() * this.getImaginary());
+    z = Math.sqrt(z);
+    return z;
+  }
+  public Complejos Prod(Complejos C)
+  {
+    Complejos returned = new Complejos();
+    returned.r_ += (this.getReal()* C.getReal());
+    returned.i_ += (this.getReal()* C.getImaginary());
+    returned.i_ += (this.getImaginary() * C.getReal());
+    returned.r_ += (-1) * (this.getImaginary() * C.getImaginary());
+    return returned;
+  }
+  public Complejos Quotient(Complejos C)
+  {
+    Complejos returned = new Complejos();
+    Complejos conjugate = C;
+    double conjugado;
+
+    if(C.i_ >= 0)
+      conjugado = (C.getReal()*C.getReal()) - (C.getImaginary()*C.getImaginary());
+    else
+      conjugado = (C.getReal()*C.getReal()) + (C.getImaginary()*C.getImaginary());
+
+    conjugate.i_ *= (-1);
+    returned = this.Prod(conjugate);
+    returned.r_ /= conjugado;
+    returned.i_ /= conjugado;
+
+    return returned;
+  }
 }
---
+++ Practicas/Practica 2/Ej 4/usaComplejos.java
@@ -0,0 +1,154 @@
+/**
+* @author Guillermo Girón García
+* @version 1.0
+* Clase que realiza operaciones con numeros Complejoss
+*/
+
+import java.util.Scanner;
+
+public class usaComplejos
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Menu: ");
+    System.out.println("1.- Sum");
+    System.out.println("2.- Substraction");
+    System.out.println("3.- Prod");
+    System.out.println("4.- Module");
+    System.out.println("5.- Quotient");
+    int option;
+    Complejos C1, C2, C3;
+    try(Scanner sc = new Scanner(System.in);)
+    {
+        option = Integer.parseInt(sc.nextLine());
+    }
+    double rP, iP;
+    switch(option)
+    {
+      case 1: System.out.println("Insert real part: ");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        rP = Double.parseDouble(sc.nextLine());
+      }
+      System.out.println("Insert imaginary part:");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        iP = sc.nextDouble();
+      }
+      C1 = new Complejos(rP, iP);
+      System.out.println("Insert real part: ");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        rP = sc.nextDouble();
+      }
+      System.out.println("Insert imaginary part:");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        iP = sc.nextDouble();
+      }
+      C2 = new Complejos(rP, iP);
+      C3 = C1.Sum(C2);
+      System.out.println(C1.getReal()+"+("+C1.getImaginary()+")i + "+
+      C2.getReal()+"+("+C2.getImaginary()+")i = "+C3.getReal()+"+("+
+      C3.getImaginary()+")i" );
+      break;
+      case 2: System.out.println("Insert real part: ");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        rP = sc.nextDouble();
+      }
+      System.out.println("Insert imaginary part:");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        iP = sc.nextDouble();
+      }
+      C1 = new Complejos(rP, iP);
+      System.out.println("Insert real part: ");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        rP = sc.nextDouble();
+      }
+      System.out.println("Insert imaginary part:");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        iP = sc.nextDouble();
+      }
+      C2 = new Complejos(rP, iP);
+      C3 = C1.Subs(C2);
+      System.out.println(C1.getReal()+"+("+C1.getImaginary()+")i - "+
+      C2.getReal()+"+("+C2.getImaginary()+")i = "+C3.getReal()+"+("+
+      C3.getImaginary()+")i" );
+      break;
+      case 3: System.out.println("Insert real part: ");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        rP = sc.nextDouble();
+      }
+      System.out.println("Insert imaginary part:");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        iP = sc.nextDouble();
+      }
+      C1 = new Complejos(rP, iP);
+      System.out.println("Insert real part: ");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        rP = sc.nextDouble();
+      }
+      System.out.println("Insert imaginary part:");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        iP = sc.nextDouble();
+      }
+      C2 = new Complejos(rP, iP);
+      C3 = C1.Prod(C2);
+      System.out.println(C1.getReal()+"+("+C1.getImaginary()+")i * "+
+      C2.getReal()+"+("+C2.getImaginary()+")i = "+C3.getReal()+"+("+
+      C3.getImaginary()+")i" );
+      break;
+      case 4: System.out.println("Insert real part: ");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        rP = sc.nextDouble();
+      }
+      System.out.println("Insert imaginary part:");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        iP = sc.nextDouble();
+      }
+      C1 = new Complejos(rP, iP);
+      double mod = C1.Mod();
+      System.out.println('|'+C1.getReal()+"+("+C1.getImaginary()+")i| = " +
+      mod);
+      break;
+      case 5: System.out.println("Insert real part: ");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        rP = sc.nextDouble();
+      }
+      System.out.println("Insert imaginary part:");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        iP = sc.nextDouble();
+      }
+      C1 = new Complejos(rP, iP);
+      System.out.println("Insert real part: ");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        rP = sc.nextDouble();
+      }
+      System.out.println("Insert imaginary part:");
+      try(Scanner sc = new Scanner(System.in);)
+      {
+        iP = sc.nextDouble();
+      }
+      C2 = new Complejos(rP, iP);
+      C3 = C1.Quotient(C2);
+      System.out.println(C1.getReal()+"+("+C1.getImaginary()+")i / "+
+      C2.getReal()+"+("+C2.getImaginary()+")i = "+C3.getReal()+"+("+
+      C3.getImaginary()+")i" );
+      break;
+      default: break;
+    }
+  }
+}
