e564573a104ea36fe2cfd5d55d0104749456e1c6
Guille31794 <guille.g94@hotmail.es>
2018-10-12 20:34:22 +0200

    Sin terminar ejercicio 2

---
+++ Ejercicios Java/Hoja 2/Ej 2/Semaforo.java
@@ -0,0 +1,16 @@
+public class Semaforo
+{
+  private String colour;
+	private int n;
+  private static final String[] colours = {"red", "yellow", "green"};
+  public Semaforo() {n = 0; colour = colours[n];}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(int n) {
+		if(n >= 0 && n < 3)
+			this.colour = colours[n];
+	}
+}
