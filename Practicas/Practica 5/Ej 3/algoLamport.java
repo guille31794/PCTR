aa4a9db2b324c1bb166ede230a2f5e28b1eb20d9
guille31794 <guille.g94@hotmail.es>
2018-10-30 21:15:31 +0100

    Realizado ejercicio 3, aunque con dudas

--- Practicas/Practica 5/Ej 3/algoLamport.java
+++ Practicas/Practica 5/Ej 3/algoLamport.java
@@ -1,69 +1,84 @@
 /* Adaptado de M. Ben-Ari por Manuel Francisco */
 
 /* Algoritmo de la panaderia para dos procesos */
-class BakeryTwo {
+class algoLamport implements Runnable
+{
     /* Iteraciones que dara cada hilo */
-    static final int iteraciones = 2000000;
+    static final int iteraciones = 2000000, N = 4;
     /* Recurso compartido */
-    static volatile int enteroCompartido = 0;
-    /* Numero para el proceso p */
-    static volatile int np = 0;
-    /* Numero para el proceso q */
-    static volatile int nq = 0;
+    static volatile int enteroCompartido = 0, it = 0;
+    /* Numero para los procesos */
+    static volatile int[] n = new int[N];
+    /* A flag for others threads while taking turn */
+    static volatile boolean[] choosing = new boolean[N];
+    /* Identifier for every thread */
+    private int id;
 
-    class P extends Thread {
-        public void run() {
-            for (int i=0; i<iteraciones; ++i) {
-                /* Seccion no critica */
-                np = nq + 1;
-                /* esperar nq = 0 o np <= nq */
-                while(nq != 0 && np > nq)
+    /* Calculate maximum element of an array */
+    private int max(int[] n)
+    {
+        int max = 0;
+
+        for(int i =0; i < N; ++i)
+            if(n[i] > max)
+                max = n[i];
+
+        return max;
+    }
+
+    public void run()
+    {
+        while(it < iteraciones)
+        {
+            /* Calculate the turn number */
+            choosing[id] = true;
+            n[id] = (1 + max(n))%N;
+            choosing[id] = false;
+
+            /* Compare with other threads */
+            for(int j = 0; j < N; ++j)
+            {
+                /* if another thread is calculating its turn, wait until it finish*/
+                while(choosing[j])
                     Thread.yield();
-               
-                Thread.yield();
-                /* Seccion critica */
-                ++enteroCompartido;
-                /* Fin Seccion critica */
-               
-                np = 0;
-            }
-        }
-    }
-    
-    class Q extends Thread {
-        public void run() {
-            for (int i=0; i<iteraciones; ++i) {
-                /* Seccion no critica */
-                nq = np + 1;
-                while(np != 0 && nq > np)
+                /* if another thread has more priority, wait until will be zero */
+                while(n[j] != 0 && n[j] <= n[id] && j < id)
                     Thread.yield();
-                
-                Thread.yield();
-                /* Seccion critica */
-                --enteroCompartido;
-                /* Fin Seccion critica */
-                
-                nq = 0;
             }
+
+            /* Critic section */
+            ++enteroCompartido; --enteroCompartido;
+            System.out.println("Resource value is: " + enteroCompartido +
+            " thread(" + id + ")");
+            /* End of critic section */
+
+            n[id] = 0;
+            ++it;
         }
     }
 
-    BakeryTwo() {
-        Thread p = new P();
-        Thread q = new Q();
-        p.start();
-        q.start();
-        
-        try {
-            p.join();
-            q.join();
-            System.out.println("El valor del recurso compartido es " + enteroCompartido);
-            System.out.println("Deberia ser 0.");
-        }
-        catch (InterruptedException e) {}
+    algoLamport(int id)
+    {
+        this.id = id;
+        n[id] = 0;
     }
 
-    public static void main(String[] args) {
-        new BakeryTwo();
+    public static void main(String[] args) throws Exception
+    {
+        Runnable[] r = new Runnable[N];
+        Thread[] t = new Thread[N];
+
+        for(int i = 0; i < N; ++i)
+        {
+            r[i] = new algoLamport(i);
+            t[i] = new Thread(r[i]);
+        }
+
+        for(int i = 0; i < N; ++i)
+        {
+            t[i].start();
+            //t[i].join();
+        }
+        System.out.println(enteroCompartido);
     }
 }
