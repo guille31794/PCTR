7e30213e2984d589ceca1470afd55a9d04a4abbf
Guille31794 <guille.g94@hotmail.es>
2018-11-19 12:46:47 +0100

    Highlight fixed

--- Practicas/Practica 6/Ej 4/resImagen.java
+++ Practicas/Practica 6/Ej 4/resImagen.java
@@ -1,3 +1,9 @@
+/**
+* @author Guillermo Girón García
+* @version 1.0
+* Class that highlight an image without concurrency
+*/
+
 /*
 * @author Guillermo Girón García
 * @version 1.0
@@ -9,45 +15,33 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.util.Random;
 
 public class resImagen
 {
-  public static int[][] load(String file) throws IOException
+  //private int start, end;
+  private static int height, wide;
+  private static BufferedImage pic;
+
+  public static void load()
   {
-      BufferedImage pic = ImageIO.read(new File(file));
-      int[][] matrix = new int[pic.getHeight()][pic.getWidth()];
+    Random r = new Random();
+    int grey;
+    for(int i = 0; i < height; ++i)
+      for(int j = 0; j < wide; ++j)
+      {
+        Color colour = new Color(r.nextInt(255));
+        grey = (int)((colour.getRed() + colour.getGreen() + colour.getBlue()) / 3);
+        pic.setRGB(i, j, (new Color(grey, grey, grey).getRGB()));
+      }
 
-      for(int i = 0 ; i < pic.getHeight() ; ++i)
-          for(int j = 0 ; j < pic.getWidth() ; ++j)
-          {
-              Color c = new Color(pic.getRGB(j, i));
-              matrix[i][j] = (int) ((c.getRed() + c.getGreen() + c.getBlue()) / 3);
-          }
-
-      return matrix;
   }
 
-  public static void save(int[][] matrix, String fichero) throws IOException
+  public static void Highlight(int height, int wide, String resultFile)
+  throws IOException
   {
-      BufferedImage pic = new BufferedImage(matrix[0].length, matrix.length, BufferedImage.TYPE_4BYTE_ABGR);
-
-      for(int i = 0 ; i < matrix.length ; ++i)
-          for(int j = 0 ; j < matrix[0].length ; ++j)
-          {
-              Color c = new Color(matrix[j][i], matrix[j][i], matrix[j][i]);
-              pic.setRGB(i, j, c.getRGB());
-          }
-
-      ImageIO.write(pic, "png", new File(fichero));
-  }
-
-  public static void Highlight(String Originalfile,
-  String resultFile) throws IOException
-  {
-      BufferedImage pic = ImageIO.read(new File(Originalfile));
-
-      for(int i = 1 ; i < pic.getHeight()-1 ; ++i)
-          for(int j = 1 ; j < pic.getWidth()-1 ; ++j)
+      for(int i = 1 ; i < height-1; ++i)
+          for(int j = 1 ; j < wide-1; ++j)
           {
               Color x = new Color(pic.getRGB(i, j));
               Color xiij = new Color(pic.getRGB(i+1, j));
@@ -64,13 +58,22 @@ public class resImagen
 
   public static void main(String[] args) throws IOException
   {
-      System.out.println("Highlighting picture");
-      Highlight("timeXCb.png", "uca_hightlighted.png");
+      height = Integer.parseInt(args[0]);
+      wide = Integer.parseInt(args[1]);
+      
+      pic =
+      new BufferedImage(height, wide, BufferedImage.TYPE_INT_RGB);
 
+      long endTime, iniTime = System.currentTimeMillis();
       System.out.println("Loading file in matrix");
-      int[][] matrix = load("uca_hightlighted.png");
+      load();
 
-      System.out.println("Saving matrix in file");
-      save(matrix, "uca_highlighted_processed.png");
+      System.out.println("Highlighting picture");
+      Highlight(height, wide, "hightlighted.png");
+      endTime = System.currentTimeMillis();
+
+      System.out.println("Matrix saved");
+      System.out.println("Time: " + (endTime-iniTime) + "ms");
+
   }
 }
