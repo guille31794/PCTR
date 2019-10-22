ce07ecb08b941c3462327febffe9915b2147ddcc
Guillermo <guille.g94@hotmail.es>
2019-10-22 15:22:19 +0200

    Extended threads creation

--- Practicas/P2/Ej 5/redCajeros.java
+++ Practicas/P2/Ej 5/redCajeros.java
@@ -8,24 +8,19 @@ public class redCajeros
     public static void main(String[] args) throws InterruptedException
     {
         cuentaCorriente c = new cuentaCorriente(1000.0);
-        cajero c1 = new cajero(c, 1, 750),
-        c2 = new cajero(c, 3, 0.0),
-        c3 = new cajero(c, 2, 375.0),
-        c4 = new cajero(c, 2, 375.0);
-        c1.start();
-        c2.start();
-        c3.start();
-        c4.start();
-        cajero c5 = new cajero(c, 1, 300);
-        c5.start();
-        cajero c6 = new cajero(c, 2, 300);
-        c5.join();
-        c6.start();
-        c6.join();
-        c4.join();
-        c3.join();
-        c2.join();
-        c1.join();
+        cajero caj[] = new cajero[9999];
+        int op = 1;
+        for(int i =  0; i < 9999; ++i)
+        {    
+            if(op == 3)
+                op -= 2;
+            caj[i] = new cajero(c,op, 3000);
+            ++op;
+            caj[i].start();
+        }
+        
+        for(int i = 9998; i > -1; --i)
+            caj[i].join();
         
         System.out.println(c.saldo);
     }
