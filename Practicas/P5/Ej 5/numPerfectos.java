970eb5aa3c2eebdf3c7a88fe98761c458e380919
Guillermo Giron Garcia <guillermogirongarcia@pop-os.localdomain>
2019-11-04 16:51:06 +0100

    P5 started

---
+++ Practicas/P5/Ej 5/numPerfectos.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.Callable;
+
+/*
+* @author Guillermo Girón García
+* @versio 1.0
+* Práctica 5: Ejercicio 5
+*/
+
+import java.util.concurrent.*;
+
+public class numPerfectos implements Callable
+{
+    public static void main(String[] args) 
+    {
+        int ini = int(args[0]), fin = new Integer(args[1]).toUnsignedLong();
+    }
+}
