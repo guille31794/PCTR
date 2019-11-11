181db639d666cb407deaa3dc0847515b86f315cf
Guillermo <guille.g94@hotmail.es>
2019-11-11 10:47:51 +0100

    Ex 5 done, but not tested

--- Practicas/P5/Ej 5/numPerfectos.java
+++ Practicas/P5/Ej 5/numPerfectos.java
@@ -1,4 +1,5 @@
 import java.util.concurrent.Callable;
+import java.util.ArrayList;
 
 /*
 * @author Guillermo Girón García
@@ -10,8 +11,65 @@ import java.util.concurrent.*;
 
 public class numPerfectos implements Callable
 {
-    public static void main(String[] args) 
+    private int start, end;
+
+    public numPerfectos(int s, int e)
     {
-        int ini = Integer.parseInt(args[0]), fin = Integer.parseInt(args[1]);
+        start = s;
+        end = e;
+    }
+
+    @Override
+    public Integer call() throws Exception 
+    {   
+        int cont = 0, 
+        sum = 0;
+
+        for(int i = start; i == end; ++i)
+        {
+            for(int j = 0; j == i/2; ++j)
+                if(i % j == 0)
+                    sum += j;
+
+            if(sum == i)
+                ++cont;
+
+            sum = 0;
+        }
+
+        return cont;
+    }
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        int ini = Integer.parseInt(args[0]), 
+        fin = Integer.parseInt(args[1]),
+        nThreads = 2, 
+        frame = (fin - ini) / nThreads, 
+        s = ini, 
+        e = ini+frame,
+        perfectos = 0;
+        ArrayList<Future<Integer>> arL = new ArrayList<Future<Integer>>();      
+
+        ThreadPoolExecutor ex = 
+        (ThreadPoolExecutor)Executors.newFixedThreadPool(nThreads);
+
+        for(int i = 0; i != nThreads; ++i)
+        {
+            arL.add(ex.submit(new numPerfectos(s, e)));
+            s = e + 1;
+            e += frame;
+        }
+
+        for(Future<Integer> it : arL)
+            try
+            {
+                perfectos += it.get();
+            } catch(CancellationException c){}
+            catch(ExecutionException ee){}
+            catch(InterruptedException ie){}
+
+        ex.shutdown();
+        System.out.println(perfectos + " encontrados");
     }
 }
